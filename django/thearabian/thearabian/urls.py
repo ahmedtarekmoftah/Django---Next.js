63e8848b3b64b1965cafe307fe9471857a231742
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-01-31 20:59:48 +0200

    Update urls.py

--- django/thearabian/thearabian/urls.py
+++ django/thearabian/thearabian/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
-from thearabian.main.views import UserViewSet, GroupViewSet
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
