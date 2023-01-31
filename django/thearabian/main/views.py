feecdf6066b306ae2181378f8c31cb88c7c4d648
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-01-31 21:12:52 +0200

    Update views.py

--- django/thearabian/main/views.py
+++ django/thearabian/main/views.py
@@ -1,4 +1,3 @@
-from thearabian.main.serializers import UserSerializer, GroupSerializer
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
