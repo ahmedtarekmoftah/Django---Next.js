3635a7a0b80240d77f2ba163e0499f8126b14ccb
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-01-31 21:42:05 +0200

    Update models.py

--- django/thearabian/main/models.py
+++ django/thearabian/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class country(models.Model):
+    name = models.CharField(max_length=64, default="Soon")
+    title = models.CharField(max_length=64, default="Soon")
+    flag = models.FileField(upload_to="main/images", null=True)
+    anthem = models.FileField(upload_to="main/audio", null=True)
+    name_ar = models.CharField(max_length=64, default="قريبا")
+    title_ar = models.CharField(max_length=64, default="قريبا")
+    capital = models.CharField(max_length=64, default="Soon")
+    population = models.CharField(max_length=64, default="Soon")
+    continent = models.CharField(max_length=64, default="Soon")
+    area = models.CharField(max_length=64, default="Soon")
+    religion = models.CharField(max_length=64, default="Soon")
+    logo = models.ImageField(upload_to="main/images", null=True)
+    capital_ar = models.CharField(max_length=64, default="قريبا")
+    continent_ar = models.CharField(max_length=64, default="قريبا")
+    religion_ar = models.CharField(max_length=64, default="قريبا")
+    location = models.CharField(max_length=1024, default="Soon")
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
