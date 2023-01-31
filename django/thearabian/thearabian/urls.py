bc0803c24efefeafee49910298005c8432fdde48
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-01-31 20:48:34 +0200

    Update urls.py

--- django/thearabian/thearabian/urls.py
+++ django/thearabian/thearabian/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
-from thearabian.main import views
+from thearabian.main.views import UserViewSet, GroupViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
